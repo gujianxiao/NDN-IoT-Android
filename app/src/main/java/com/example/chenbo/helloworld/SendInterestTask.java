63164e1b5525e7d36ae5e613eccb0381e9e95a2e
Edward Lu <edwardzlu98@gmail.com>
2019-01-14 22:55:39 -0800

    Moved the ndn lite android support library into the application, also updated application to work with latest version of ndn-lite

--- app/src/main/java/com/example/chenbo/helloworld/SendInterestTask.java
+++ app/src/main/java/com/example/chenbo/helloworld/SendInterestTask.java
@@ -1,3 +1,12 @@
+
+/*
+ * Copyright (C) 2018-2019 Bo Chen
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
 package com.example.chenbo.helloworld;
 
 import android.os.AsyncTask;
